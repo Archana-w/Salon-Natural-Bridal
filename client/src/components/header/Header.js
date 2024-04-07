d812bf9a91345e57a5e6b2e4f06f39b5b366096d
Chirathi Somadasa <chirathisomadasa@gmail.com>
2024-04-07 12:32:31 +0530

    update nav bar

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -7,43 +7,56 @@ import Logo from '../../images/logo.png';
 import ProfileVector from '../../images/default_profile_vector.webp';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-function Header(){
+function Header() {
 
     const [activePage, setActivePage] = useState(null);
     var location = useLocation();
     var navigate = useNavigate();
 
-    useEffect(function(){
+    useEffect(function () {
         setActivePage(getPageId(location.pathname));
     });
 
-    function navItemClick(id){
+    function navItemClick(id) {
         navigate(id);
     }
 
-    function onCartButtonClick(){
+    function onCartButtonClick() {
+        navigate("signup");
+    }
+    function onSignUpButtonClick() {
+        navigate("login");
+    }
+    function onLoginButtonClick() {
+        navigate("notification");
+    }
+    function onProfileButtonClick() {
         navigate("cart");
     }
-
-    return(
+    return (
         <div className="header">
             <div className="header-top">
                 <div className="logo">
                     <img src={Logo} />
                 </div>
                 <div className="header-right">
-                    
-                    
+
+                    <div className='div-btn'id="signup" activeId={activePage} onClick={onSignUpButtonClick}>
+                        <button className='signup-button'>Sign Up</button>
+                    </div>
+                    <div className='div-btn' id="login" activeId={activePage} onClick={onLoginButtonClick}>
+                        <button className='signup-button'>Login</button>
+                    </div>
                     <HeaderButton id="notification" activeId={activePage} onClick={onCartButtonClick}>
                         <span className="material-icons-outlined">notifications</span>
                     </HeaderButton>
-                    <HeaderButton id="cart" activeId={activePage} onClick={onCartButtonClick}>
+                    <HeaderButton id="cart" activeId={activePage} onClick={onProfileButtonClick}>
                         <span className="material-icons-outlined">shopping_bag</span>
                     </HeaderButton>
 
                     <div className="profile">
                         <div className="profile-picture">
-                            <img src={ProfileVector}/>
+                            <img src={ProfileVector} />
                         </div>
                     </div>
 
@@ -51,23 +64,23 @@ function Header(){
             </div>
             <div className="header-nav">
 
-                <HeaderNavButton id="" activeId={activePage} name="Home" onClick={(id) => {navItemClick(id)}} />
+                <HeaderNavButton id="" activeId={activePage} name="Home" onClick={(id) => { navItemClick(id) }} />
                 <HeaderNavButton id="create-app" activeId={activePage} name="Create Appoinment" onClick={(id) => { navItemClick(id) }} />
                 <HeaderNavButton id="store" activeId={activePage} name="Our Store" onClick={(id) => { navItemClick(id) }} />
                 <HeaderNavButton id="about" activeId={activePage} name="About Us" onClick={(id) => { navItemClick(id) }} />
                 <HeaderNavButton id="contact" activeId={activePage} name="Contact Us" onClick={(id) => { navItemClick(id) }} />
- 
+
             </div>
         </div>
     );
 }
 
-function getPageId(path){
-    path = path.substring(1,path.length);
+function getPageId(path) {
+    path = path.substring(1, path.length);
     const firstIndex = path.indexOf("/");
-    if(firstIndex == -1){
+    if (firstIndex == -1) {
         return path;
-    }else{
+    } else {
         return path.substring(0, firstIndex);
     }
 }
