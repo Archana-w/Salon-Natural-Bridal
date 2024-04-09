0e0689d54650f0f19b76d50bfb3783150a291b0e
Udeshika Balasooriya <piumiudeshika02@gmail.com>
2024-04-09 14:42:09 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header'
+
 import SignUp from './pages/signup/SignUp';
 import Login from './pages/login/Login';
 import SignOut from './pages/signout/SignOut';
+
 
 function App() {
   
@@ -15,9 +17,11 @@ function App() {
       <Header />
 
       <Routes>
+
         <Route path="/signup" Component={SignUp} />
         <Route path="/login" Component={Login} />
         <Route path="signout" Component={SignOut}/>
+
       </Routes>
 
     </BrowserRouter>
