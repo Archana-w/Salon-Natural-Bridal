495e0f4962cdb7ec275addbe526935b6a5a4cf00
savinduonline <savinduen@gmail.com>
2024-04-09 13:03:21 +0530

    Change admin auth cookies(3rd)

--- admin/src/auth.js
+++ admin/src/auth.js
@@ -3,7 +3,7 @@ import { useCookies } from 'react-cookie';
 function useAuthToken(){
 
     const [cookies, setCookies] = useCookies(["admin_auth_token"]);
-    var authToken = cookies.auth_token;
+    var authToken = cookies.admin_auth_token;
     
     if(authToken == undefined){
         return null;
