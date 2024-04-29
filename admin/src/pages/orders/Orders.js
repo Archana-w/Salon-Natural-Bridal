26859413aa265bbacebcefd78a3c44aaa9ad9e3f
savinduonline <savinduen@gmail.com>
2024-04-29 17:51:48 +0530

    Change

--- admin/src/pages/orders/Orders.js
+++ admin/src/pages/orders/Orders.js
@@ -11,30 +11,7 @@ function Orders() {
 
    useEffect(() => {
 
-      /*
-      if (token != null) {
-
-         axios.post("http://localhost:5000/emp/get", { token: token }).then((response) => {
-
-            var data = response.data;
-            var status = data.status;
-            if (status == "success") {
-               setEmployeeData(data.data);
-            } else if (status == "token_expired" || status == "auth_failed") {
-               navigate("/signout");
-            } else {
-               var message = data.message;
-               alert("Error - " + message);
-            }
-
-         }).catch((error) => {
-            alert("Error 2 - " + error);
-         });
-
-      } else {
-         navigate("/signout");
-      }
-      */
+      
 
    });
 
