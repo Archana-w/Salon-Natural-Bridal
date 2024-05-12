ace836587c5699f4fcb9670a014b3a1df418b280
K-G-C-N-Kulathunga <161619772+K-G-C-N-Kulathunga@users.noreply.github.com>
2024-05-12 17:25:13 +0530

    service_css

--- admin/src/pages/service/EditServiceForm.js
+++ admin/src/pages/service/EditServiceForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./EditServiceForm.css"; // Import your CSS file
 
-function EditServiceForm({ service, onUpdate }) {
+function EditServiceForm({ service, onUpdate, onCancel }) {
     const [editedService, setEditedService] = useState(service);
 
     const handleChange = (e) => {
@@ -23,25 +23,31 @@ function EditServiceForm({ service, onUpdate }) {
             });
     };
 
+    const handleCancel = (e) => {
+        e.preventDefault();
+        onCancel(); // Call onCancel function provided by parent component to close the edit form
+    };
+
     return (
         <form onSubmit={handleSubmit} className="edit-service-form">
-            <div className="form-group">
+            <div className="service-form-group">
                 <label htmlFor="sType" className="form-label">Service Type</label>
                 <input type="text" id="sType" name="sType" className="form-control" value={editedService.sType} onChange={handleChange} />
             </div>
-            <div className="form-group">
+            <div className="service-form-group">
                 <label htmlFor="sName" className="form-label">Service Name</label>
                 <input type="text" id="sName" name="sName" className="form-control" value={editedService.sName} onChange={handleChange} />
             </div>
-            <div className="form-group">
+            <div className="service-form-group">
                 <label htmlFor="sPrice" className="form-label">Price</label>
                 <input type="text" id="sPrice" name="sPrice" className="form-control" value={editedService.sPrice} onChange={handleChange} />
             </div>
-            <div className="form-group">
+            <div className="service-form-group">
                 <label htmlFor="sDescription" className="form-label">Description</label>
                 <textarea id="sDescription" name="sDescription" className="form-control" value={editedService.sDescription} onChange={handleChange} />
             </div>
-            <button type="submit" className="btn btn-primary">Update</button>
+            <button type="submit" className="service-btn-primary">Update</button>
+            <button onClick={handleCancel} className="service-btn-primary">Cancel</button>
         </form>
     );
 }
